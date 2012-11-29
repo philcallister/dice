8ac9bed27af4ebf1359a542b3303959d0cadc4ae
Phil Callister <philcallister@gmail.com>
2012-11-28 23:06:02 -0600

    LPeg Test...needed for dice parsing

--- main.lua
+++ main.lua
@@ -17,3 +17,37 @@ local d3 = Dice:parse("4d6 2d4")
 d1.out()
 d2.out()
 d3.out()
+
+
+
+-- @@@@@ Quick LPEG Test: v2012.971 @@@@
+
+local lpeg = require"lpeg"
+
+-- Lexical Elements
+local Space = lpeg.S(" \n\t")^0
+local Number = lpeg.C(lpeg.P"-"^-1 * lpeg.R("09")^1) * Space
+local FactorOp = lpeg.C(lpeg.S("+-")) * Space
+local TermOp = lpeg.C(lpeg.S("*/")) * Space
+local Open = "(" * Space
+local Close = ")" * Space
+
+-- Auxiliary function
+function eval (v1, op, v2)
+  if (op == "+") then return v1 + v2
+  elseif (op == "-") then return v1 - v2
+  elseif (op == "*") then return v1 * v2
+  elseif (op == "/") then return v1 / v2
+  end
+end
+
+-- Grammar
+local V = lpeg.V
+G = lpeg.P{ "Exp",
+  Exp = lpeg.Cf(V"Factor" * lpeg.Cg(FactorOp * V"Factor")^0, eval);
+  Factor = lpeg.Cf(V"Term" * lpeg.Cg(TermOp * V"Term")^0, eval);
+  Term = Number / tonumber + Open * V"Exp" * Close;
+}
+
+-- small example
+print(">>>>> LPeg Test: ", lpeg.match( G, "3 + 5*9 / (1+1) - 12") )
